059884a25fb36d759cca50567f38055a7b4f8047
Tim Welch <tim.j.welch@gmail.com>
2024-12-20 12:40:52 -0800

    June 2024 update - keep more properties in marine ecoregions

--- data/eez-mr-v12-prep.sh
+++ data/eez-mr-v12-prep.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Expects that global-eez-mr-v12 has already been imported.  This is a subdivided variation
 
-ogr2ogr -f FlatGeobuf -explodecollections -dialect sqlite -sql "select GEONAME, MRGID, ST_Subdivide(geometry, 512) from eez_v12" data/dist/global-eez-mr-v12-subdivided.fgb data/dist/global-eez-mr-v12.fgb
+rm -f data/dist/global-eez-mr-v12-subdivided.fgb
+ogr2ogr -f FlatGeobuf -explodecollections -dialect sqlite -sql "select GEONAME, MRGID, ST_buffer(ST_Subdivide(geometry, 512), 0) from eez_v12" data/dist/global-eez-mr-v12-subdivided.fgb data/dist/global-eez-mr-v12.fgb
